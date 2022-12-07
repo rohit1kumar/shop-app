8c6afc1a8a693172600135a704041128161780f7
Rohit Kumar <sonu12345kumar12345@gmail.com>
2022-12-07 21:27:15 +0530

    fix: add port to db

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const DATABASE_NAME = process.env.DATABASE_NAME;
-const DATABASE_USER = process.env.DATABASE_USER;
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
-const DATABASE_HOST = process.env.DATABASE_HOST;
+const DATABASE_NAME = process.env.DATABASE_NAME || 'shopdb';
+const DATABASE_USER = process.env.DATABASE_USER || 'postgres';
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || 'postgres';
+const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost';
+const DATABASE_PORT = process.env.DATABASE_PORT || 5432;
 
 const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, {
     host: DATABASE_HOST,
+    port: DATABASE_PORT,
     dialect: 'postgres',
     logging: false
 });
